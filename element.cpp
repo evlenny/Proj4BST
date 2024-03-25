d162f2f1aeb65be63337d6c74ec626323ac5b86e
GraceLock <lock_g1@denison.edu>
2024-03-25 16:36:50 -0400

    element .h and .cpp

--- element.cpp
+++ element.cpp
@@ -1,1 +1,91 @@
+#include <sstream>
+#include <iostream>
+#include "element.h"
 
+//=========================================================================
+// base constructor
+// Parameters: none
+// Returns:	new Element object
+// Creates an empty Element object
+//=========================================================================
+template <class T>
+Element<T>::Element() : item(T()), key(-1) {
+    // Default constructor of T is used to initialize item
+    // For int, it initializes to 0 (using std::numeric_limits), for std::string, it initializes to empty string
+}
+// Pre condition: None
+// Post condition: An empty element is created
+/*
+{
+    if (constexpr (std::is_same<T, int>)) {
+        item = 0;
+    } else if (constexpr (std::is_same<T, std::string>)) {
+        item = "";
+    } else {
+        // Handle other types here if necessary
+    }
+    key = -1;
+}
+*/
+
+//=========================================================================
+// parameter constructor
+// Parameters: 
+//  d - data value for the element
+//  k - key for the element
+// Returns:	none
+//=========================================================================
+template <class T>
+Element<T>::Element( T d, int k )
+// Pre condition: d and k are valid entries for the element type
+// Post condition: an element is created with d as the data and k as the key 
+{
+	item = d;
+	key = k;
+}
+
+
+//=========================================================================
+// get_key
+// Parameters: none
+// Returns:	
+//  key - int value of the element's key 
+//=========================================================================
+template <class T>
+int Element<T>::get_key( void ) const
+// Pre condition: element exists 
+// Post condition: correct key vlaue of the element is returned  
+{
+    return key;
+}
+
+
+//=========================================================================
+// get_data
+// Parameters: none
+// Returns:	
+//  item - data value of the element 
+//=========================================================================
+template <class T>
+T Element<T>::get_data( void ) const
+// Pre condition: element exists 
+// Post condition: correct data value of the element is returned 
+{
+    return item;
+}
+
+//=========================================================================
+// to_string
+// Parameters: none
+// Returns:	
+//  string - the string containing the data and key of an element 
+//=========================================================================
+template <class T>
+std::string Element<T>::to_string() const 
+// Pre condition: element exists 
+// Post condition: string contains correct data and key value of the element
+{
+    ostringstream out;
+    out << "(" << item << "," << key << ")";
+    return out.str();
+}
--- element.h
+++ element.h
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <chrono>
 
+using namespace std;
+
+// Header guards
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+// Declaring element template class
+template <class T>
+class Element 
+{
+private:
+        T item;
+        int key;
+
+public:
+    Element( void ); // empty constructor
+    Element( const T data, const int k ); // parameter constructor
+
+    int     get_key( void ) const;
+    T       get_data( void ) const;
+    string  to_string( void ) const;
+};
+
+#endif
