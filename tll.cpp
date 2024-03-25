59366f97569857572508254c5f822aaa89c5d648
GraceLock <lock_g1@denison.edu>
2024-03-25 16:46:17 -0400

    triply linked list .h .cpp

--- tll.cpp
+++ tll.cpp
@@ -1,1 +1,5 @@
+#include <iostream>
+#include <string>
+#include "linkedlist.h"
 
+using namespace std;
--- tll.h
+++ tll.h
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "element.cpp"
 
+using namespace std;
+
+#ifndef TRIPLYLINKEDLIST_H
+#define TRIPLYLINKEDLIST_H
+
+template <class T> 
+class linkedlist
+{
+private:
+    struct Node
+    {
+        Element<T> item;
+        Node *parent;
+        Node *left;
+        Node *right;
+    };
+    
+    Node *head;          
+
+public:
+    linkedlist( void );
+    void insert( const Element<T> &elem );
+    void remove(const int k);
+    string to_string( void ) const; 
+    bool search(const Element<T> &elem) const;
+    bool is_empty( void );
+};
+
+#endif
